e2d9d2ce6bc4b3f8f909c83fe1c30dc1947605d6
Yair Rodríguez <omar.rodriguez@anconaautopartes.com>
2021-11-30 17:14:09 -0500

    Base URL changed

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "https://localhost:44354/api/v1"; // "https://api.anconaautopartes.com/api/v1.0";
-// const DEV_TOKEN = "";
+const BASE_URL = "https://api.anconaautopartes.com/api/v1.0";
 
 export const axiosInstance = axios.create({
     baseURL: BASE_URL,
