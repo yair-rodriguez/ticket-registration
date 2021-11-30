4700403115d4c1f46b67cbc1331055d3c103c2a8
Yair Rodríguez <omar.rodriguez@anconaautopartes.com>
2021-11-30 16:31:01 -0500

    server folder removed

--- src/server/endpoints/offerts.ts
+++
@@ -1,8 +0,0 @@
-/* import { axiosInstance } from "../../api/axios";
-import { IPostOffertsRequest, IPostOffertsResponse } from "../../interfaces/offerts";
-
-export const postOfferts = async (req: IPostOffertsRequest): Promise<IPostOffertsResponse> => {
-    const res = await axiosInstance.post(`/offerts`, req);
-    const resData = res.data as IPostOffertsResponse;
-    return Promise.resolve(resData);
-}; */
